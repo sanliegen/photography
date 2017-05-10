8fd388c8127e533b41a0d5dd2b94b24c0a305787
denniszyche <dennis.zyche@gmail.com>
2017-05-10 14:16:09 +0200

    Add link-color-var->header

--- wp-content/themes/sanliegen photography theme/header.php
+++ wp-content/themes/sanliegen photography theme/header.php
@@ -7,6 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
+    <style type="text/css">
+      a:focus { color: <?php the_field('link_color', 'option'); ?>;}
+      a:hover { color: <?php the_field('link_color', 'option'); ?>; }
+      a:active { color: <?php the_field('link_color', 'option'); ?>; }
+      .contact__us__form__field .contact__us__form__group input { border-color: <?php the_field('link_color', 'option'); ?>; }
+      .contact__us__form__field .contact__us__form__group textarea { border-color: <?php the_field('link_color', 'option'); ?>; }
+      .contact__form__button { background: <?php the_field('link_color', 'option'); ?>; }
+    </style>
   </head>
 
 
