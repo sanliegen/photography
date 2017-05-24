b155c6a2b36ecf3972827af1194c25c7a852edc1
denniszyche <dennis.zyche@gmail.com>
2017-05-24 18:44:59 +0200

    Upload Update

--- wp-content/themes/sanliegen photography theme/assets/js/app.js
+++ wp-content/themes/sanliegen photography theme/assets/js/app.js
@@ -1,33 +1,3 @@
-$(function() {
-  // horizontalGallery();
-});
-function horizontalGallery(){
-  checkSize();
-  $(window).resize(checkSize);
-}
-
-function checkSize(){
-    overallwidth = 0;
-
-    $('.horizontal__gallery__box').each(function() {
-        overallwidth = overallwidth + $(this).width() + 100;
-    });
-
-    $('.horizontal__gallery__wrap').mousewheel(function(event, delta) {
-      this.scrollLeft -= (delta * 1);
-      event.preventDefault();
-    });
-
-    if ($(".horizontal__gallery__box").css("float") == "none" ){
-      $('.horizontal__gallery').css('width', '100%');
-      $('.horizontal__gallery__wrap').unbind("mousewheel");
-    } else {
-      $('.horizontal__gallery').css('width', overallwidth);
-    }
-
-}
-
-
 $(window).on("load resize",function() {
   imgWidth = 0;
   $('.js-image-width').each(function(){
--- wp-content/themes/sanliegen photography theme/inc/enqueue.php
+++ wp-content/themes/sanliegen photography theme/inc/enqueue.php
@@ -19,13 +19,13 @@ function frontend_load_scripts() {
   wp_enqueue_style( 'socialfonts', get_template_directory_uri() . '/assets/socialfonts/fonts.min.css', array(), '1.0.0', 'all');
   wp_enqueue_style( 'bootstrapGrid', get_template_directory_uri() . '/assets/bootstrap.min.css', array(), '1.0.0', 'all');
   wp_enqueue_style( 'anacatala', get_template_directory_uri() . '/assets/style.css', array(), '1.0.0', 'all');
-  wp_enqueue_style( 'mfpcss', get_template_directory_uri() . '/assets/mfp.css', array(), '1.0.0', 'all');
+  wp_enqueue_style( 'mfpcss', get_template_directory_uri() . '/assets/mfp.min.css', array(), '1.0.0', 'all');
   wp_deregister_script( 'jquery' );
   wp_register_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery.js', false, '1.11.3', true);
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'mfp', get_template_directory_uri() . '/assets/js/magnific-popup.min.js', array(), '1.0.0', true);
   wp_enqueue_script( 'mousewheel', get_template_directory_uri() . '/assets/js/mousewheel.js', array(), '1.0.0', true);
   wp_enqueue_script( 'functions', get_template_directory_uri() . '/assets/js/functions.js', array(), '1.0.0', true);
-  wp_enqueue_script( 'app', get_template_directory_uri() . '/assets/js/app.js', array(), '1.0.0', true);
+  wp_enqueue_script( 'app', get_template_directory_uri() . '/assets/js/app.min.js', array(), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'frontend_load_scripts' );
